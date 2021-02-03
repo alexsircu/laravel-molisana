1f9af8c91373e5d367c2c6f9eb37b6c136dd1202
Alex Sircu <alex.sricu@gmail.com>
2021-02-03 22:23:06 +0100

    Main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
 
         <title>Laravel</title>
 
-        <!-- Css -->
+        <!-- CSS -->
         <link rel="stylesheet" href="{{ asset("css/app.css") }}">
 
     </head>
@@ -25,13 +25,45 @@
        <main>
 
          <div class="container">
-           <h2>LE LUNGHE</h2>
+            <h2>LE LUNGHE</h2>
+            <div class="card-container">
+              @foreach ($pasta_lunga as $pasta)
+                <div class="card">
+                  <img src="{{ $pasta["src"] }}" alt="{{ $pasta["titolo"] }}">
+                </div>
+              @endforeach
+            </div>
+         </div>
 
+         <div class="container">
+            <h2>LE CORTE</h2>
+            <div class="card-container">
+              @foreach ($pasta_corta as $pasta)
+                <div class="card">
+                  <img src="{{ $pasta["src"] }}" alt="{{ $pasta["titolo"] }}">
+                </div>
+              @endforeach
+           </div>
+         </div>
+
+         <div class="container">
+            <h2>LE CORTISSIME</h2>
+            <div class="card-container">
+              @foreach ($pasta_cortissima as $pasta)
+                <div class="card">
+                  <img src="{{ $pasta["src"] }}" alt="{{ $pasta["titolo"] }}">
+                </div>
+              @endforeach
+           </div>
          </div>
 
        </main>
 
        <footer>
+
+        <div class="container">
+
+        </div>
 
        </footer>
     </body>
