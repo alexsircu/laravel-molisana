b340c70222aac44a4de4937cf6047e7e2d2b12a8
Alex Sircu <alex.sricu@gmail.com>
2021-02-07 20:55:46 +0100

    Stile pagina prodotto completo

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -5,6 +5,10 @@
   <div id="product-container">
     <h1 id="products-title">{{ $product["titolo"] }}</h1>
     <img src="{{ $product["src-h"] }}" alt="{{ $product["titolo"] }}">
+    <img src="{{ $product["src-p"] }}" alt="{{ $product["titolo"] }}" id="product-pack"> 
+    <div class="container">
+      <p>{!! $product["descrizione"] !!}</p>
+    </div>
   </div>
 </main>    
 @endsection
